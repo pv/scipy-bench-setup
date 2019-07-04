1bfba339dc0b4a8eb7fb25011d3b26025b715e1f
Pauli Virtanen <pav@iki.fi>
2019-07-04 18:56:59 +0200

    Add --durations

--- cron.sh
+++ cron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname "$0")
 source $HOME/env/bin/activate
-asv-bwrap --upload --lock=$HOME/lock config.toml run NEW --cpu-affinity=3 -k -e --steps=11
+asv-bwrap --upload --lock=$HOME/lock config.toml run NEW --cpu-affinity=3 -k -e --steps=11 --durations
