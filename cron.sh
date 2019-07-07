6151ee302dac1fd0d64b4d8c92fc279f5a5bc52f
Pauli Virtanen <pav@iki.fi>
2019-07-07 22:11:15 +0200

    Fewer builds to catchup

--- cron.sh
+++ cron.sh
@@ -2,4 +2,4 @@
 set -e -x
 cd $(dirname "$0")
 source $HOME/env/bin/activate
-asv-bwrap --upload --lock=$HOME/lock config.toml run NEW --cpu-affinity=3 -k -e --steps=11 --durations
+asv-bwrap --upload --lock=$HOME/lock config.toml run NEW --cpu-affinity=3 -k -e --steps=6 --durations
