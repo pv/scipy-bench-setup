f6cdf660f6db7c8979864b69c0f608a2ffa15a85
Pauli Virtanen <pav@iki.fi>
2019-07-14 16:01:01 +0200

    populate.sh: show durations

--- populate.sh
+++ populate.sh
@@ -2,4 +2,4 @@
 set -v -e -x
 cd $(dirname "$0")
 source $HOME/env/bin/activate
-exec asv-bwrap --upload --lock=$HOME/lock config.toml run --cpu-affinity=3 --date-period=30d -e -k v0.17.0..origin/master
+exec asv-bwrap --upload --lock=$HOME/lock config.toml run --cpu-affinity=3 --date-period=30d --durations=all -e -k v0.17.0..origin/master
