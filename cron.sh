16f95aa14cac6c22cd49bed6d4e94c76c7e7c5c8
Pauli Virtanen <pav@iki.fi>
2020-08-16 14:26:12 +0300

    Run xslow benchmarks on a 20-day schedule

--- cron.sh
+++ cron.sh
@@ -2,4 +2,5 @@
 set -e -x
 cd $(dirname "$0")
 source $HOME/env/bin/activate
-asv-bwrap --upload --lock=$HOME/lock config.toml run NEW --cpu-affinity=3 -k -e --steps=6 --durations
+asv-bwrap --lock=$HOME/lock config.toml run NEW --cpu-affinity=3 -k -e --steps=6 --durations
+asv-bwrap --upload --lock=$HOME/lock config.toml xslow run --cpu-affinity=3 --date-period=20d --durations=all -e -k "--first-parent f7f31b45..origin/master"
