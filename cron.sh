b9656b852dfc0da00e94f26fd356006177e6ecca
Pauli Virtanen <pav@iki.fi>
2021-06-15 21:20:39 +0000

    Fix repo branch

--- cron.sh
+++ cron.sh
@@ -2,5 +2,6 @@
 set -e -x
 cd $(dirname "$0")
 source $HOME/env/bin/activate
-asv-bwrap --lock=$HOME/lock config.toml xslow run --cpu-affinity=3 --date-period=20d --durations=all -e -k "--first-parent v1.10.0^..origin/master"
+asv-bwrap --upload --lock=$HOME/lock config.toml run origin/main^! --cpu-affinity=3 -k -e --durations
+asv-bwrap --lock=$HOME/lock config.toml xslow run --cpu-affinity=3 --date-period=20d --durations=all -e -k "--first-parent v1.10.0^..origin/main"
 asv-bwrap --upload --lock=$HOME/lock config.toml run NEW --cpu-affinity=3 -k -e --steps=6 --durations
