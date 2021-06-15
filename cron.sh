252fcbae1333a10a81df1a7c91c4df4a82baf877
Pauli Virtanen <pav@iki.fi>
2021-06-15 22:01:36 +0000

    Fix

--- cron.sh
+++ cron.sh
@@ -2,6 +2,5 @@
 set -e -x
 cd $(dirname "$0")
 source $HOME/env/bin/activate
-asv-bwrap --upload --lock=$HOME/lock config.toml run origin/main^! --cpu-affinity=3 -k -e --durations
 asv-bwrap --lock=$HOME/lock config.toml xslow run --cpu-affinity=3 --date-period=20d --durations=all -e -k "--first-parent v1.10.0^..origin/main"
 asv-bwrap --upload --lock=$HOME/lock config.toml run NEW --cpu-affinity=3 -k -e --steps=6 --durations
